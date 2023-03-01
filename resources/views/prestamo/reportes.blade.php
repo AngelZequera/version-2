347b525384219ed7577f7165f4a65f933f8465d1
Angel Zequera <misteriangel144@gmail.com>
2023-03-01 11:55:51 -0600

    modificacion de titulo de reportes

--- resources/views/prestamo/reportes.blade.php
+++ resources/views/prestamo/reportes.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Reportes</title>
+    <title>Reporte de préstamo {{$cargo}}</title>
   
 <!-- Funcion del diagrama cargos-->
     <script src="{{ asset('js/Chart.js') }}"></script>
